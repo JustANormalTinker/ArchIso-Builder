c4dc30f71293a29b12936c267ee9966bcc4e3ce1
JustANormalTinker <105440921+JustANormalTinker@users.noreply.github.com>
2023-04-17 18:47:59 +0530

    Fix upload script

--- upload.sh
+++ upload.sh
@@ -1,4 +1,4 @@
-pacman -S rclone --noconfirm
+pacman -S rclone wget --noconfirm
 wget https://pastebin.com/raw/PJ4b9SMw -O ~/.config/rclone/rclone.conf
 rclone copy output/*.iso drive:
 curl --upload-file output/*.iso https://free.keep.sh
